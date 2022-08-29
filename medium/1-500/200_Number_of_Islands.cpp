497916692cfc987563dab94422407b53bdd681b9
Lyn <2395781272@qq.com>
2022-08-29 10:38:21 +0800

    mod

--- medium/1-500/200_Number_of_Islands.cpp
+++ medium/1-500/200_Number_of_Islands.cpp
@@ -20,50 +20,17 @@ public:
         grid[x][y] = '0';
         for(int d = 0; d < 4; d++) {
             int a = x + dir[d], b = y + dir[d + 1];
-            if((a >= 0) && (a < m) && (b >= 0) && (b < n) && grid[a][b] == '1') {
-                dfs(grid, a, b, m, n);
-            }
+            dfs(grid, a, b, m, n);
         }
-        /*
-        dfs(grid, x + 1, y, m, n);
-        dfs(grid, x, y - 1, m, n);
-        dfs(grid, x - 1, y, m, n);
-        dfs(grid, x, y + 1, m, n);
-        */
-
-
-                for(int d = 0; d < 4; d++) {
-                    int a = x + dir[d], b = y + dir[d + 1];
-                    //cout << a << endl;
-                    if((a >= 0) && (a < m) && (b >= 0) && (b < n) && grid[a][b] == '1') {
-                        //grid[x][y] == '0';
-                        dfs(grid, a, b, m, n);
-                    }
-                }
-
-        //for(int i = 0; i < m; i++) {
-            //for(int j = 0; j < n; j++) {
-            //if(x < m && y < n && grid[x][y] == '1') {
-                    //cout << "xy" << grid[x][y] << endl;
-                //if(grid[i][j] != '1') return 0;
-
-
-
-            //}
-        //}
     }
 
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size(), sum = 0;
-        //cout << m << n << endl;
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                    //cout << grid[i][j] << endl;
                 if(grid[i][j] == '1') {
                     sum++;
-                    cout << sum << endl;
                     dfs(grid, i, j, m, n);
-
                 }
             }
         }
