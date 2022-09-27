6a76ad96fc0c276af638e1f0c502bab0af490879
Lyn <2395781272@qq.com>
2022-09-27 11:35:37 +0800

    mod 990

--- medium/500-1000/990_Satisfiability_of_Equality_Equations.cpp
+++ medium/500-1000/990_Satisfiability_of_Equality_Equations.cpp
@@ -18,27 +18,26 @@ class Solution {
 public:
 
     int uf[26];
+
     int find(int x) {
-        int k = uf[x] == x ? x : (uf[x] = find(uf[x]));
-        cout << "k:" << k << endl;
-        return k;
+        if (x != uf[x]) uf[x] = find(uf[x]);
+        return uf[x];
     }
 
     bool equationsPossible(vector<string>& equations) {
-        for (int i = 0; i < 26; ++i) uf[i] = i;
+        for (int i = 0; i < 26; i++) uf[i] = i;
 
-        for (auto e : equations) {
-            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a');
-        }
-        cout << "---" << endl;
-
-        //for (auto x : uf) cout << x << endl;
-
-        for (auto e : equations) {
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
+        for (auto eq : equations) {
+            if (eq[1] == '=') {
+                uf[find(eq[0] - 'a')] = find(eq[3] - 'a');
+            }
         }
 
-
+        for (auto eq : equations) {
+            if (eq[1] == '!' && find(eq[0] - 'a') == find(eq[3] - 'a')) {
+                return false;
+            }
+        }
         return true;
     }
 };
